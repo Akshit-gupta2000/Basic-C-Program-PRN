2ba0099b45e59da012292c8395fe13403988be23
Akshit-gupta2000 <54929348+Akshit-gupta2000@users.noreply.github.com>
2019-10-17 21:17:28 +0530

    Add files via upload

---
+++ Printing PRN.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	char a[20];
+	printf("Enter your PRN:");
+	scanf("%s",&a);
+	printf("Your PRN is:%s",&a);
+	
+}
